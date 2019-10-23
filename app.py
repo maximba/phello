792b573aeef5fff14901e6bb27641205be903915
maximba <maxi@bxsoft.com>
2019-10-23 14:24:42 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+bind_addr = "0.0.0.0"
+port = "8013"
+
+@app.route("/")
+def hello():
+  return "Hello GitLab from python!"
+  
+if __name__ == "__main__":
+  app.run(host=bind_addr, port=8013)
+  
